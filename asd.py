df01be4fbc11ef8268ee0271f238558aa6bd0160
shakhrizoda <sahrizodasaidazizova@gmail.com>
2024-06-10 16:21:39 +0500

    asd.py

--- asd.py
+++ asd.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import random
-
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-
-for unique_value in data['whoAmI'].unique():
-    data[unique_value] = data['whoAmI'].apply(lambda x: 1 if x == unique_value else 0)
-
-data.head()
+unique_values = data['whoAmI'].unique()
+for value in unique_values:
+   data[value] = (data['whoAmI'] == value).astype(int)
+data.drop('whoAmI', axis=1, inplace=True)
+print(data.head())
